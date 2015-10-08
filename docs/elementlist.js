bbe7aa92c99921067fdd9583900882a3092fc8cf
Bishop Bettini <bishop.bettini@gmail.com>
2015-10-08 15:58:32 -0400

    Removes local documentation

--- docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Haldayne\\Customs\\AbstractUploadEntity"],["c","Haldayne\\Customs\\UploadException"],["c","Haldayne\\Customs\\UploadFailure"],["c","Haldayne\\Customs\\UploadFile"],["c","Haldayne\\Customs\\UploadIterator"]];
