daf85ce935c94ceead075e9889d362e58dc6a8b8
Bishop Bettini <bishop.bettini@gmail.com>
2015-10-08 15:39:12 -0400

    Adds generated docs.

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Haldayne\\Customs\\AbstractUploadEntity"],["c","Haldayne\\Customs\\UploadException"],["c","Haldayne\\Customs\\UploadFailure"],["c","Haldayne\\Customs\\UploadFile"],["c","Haldayne\\Customs\\UploadIterator"]];
